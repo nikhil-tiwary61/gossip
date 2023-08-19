8a2f4cea48861a7f87a46353a9784a57f39fcd79
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-20 04:47:57 +0530

    style: add css to UserBoard component
    feat: add google profile photo

--- src/components/UserBoard.js
+++ src/components/UserBoard.js
@@ -1,10 +1,13 @@
 import "./UserBoard.css";
 
 export default function UserBoard({ user }) {
+  const photoURL = user.photoURL;
   return (
     <div className="user-panel">
-      <p>Welcome</p>
-      <h3>{user.name}</h3>
+      {/* bug fix : google image dosent load */}
+      {/* <img src={photoURL} alt="Google User" className="user-image" /> */}
+      <h2 className="greeting">Welcome</h2>
+      <h3 className="userName">{user.name}</h3>
     </div>
   );
 }
