15c1358391afd4ba2a559f4b1c01203f8d08f475
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-22 13:58:59 +0530

    add style to UserBoard component

--- src/components/UserBoard.js
+++ src/components/UserBoard.js
@@ -1,13 +1,15 @@
 import "../styles/UserBoard.css";
 
 export default function UserBoard({ user }) {
-  const photoURL = user.photoURL;
+  // const photoURL = user.photoURL;
   return (
     <div className="user-panel">
-      {/* bug fix : google image dosent load */}
-      {/* <img src={photoURL} alt="Google User" className="user-image" /> */}
-      <h2 className="greeting">Welcome</h2>
-      <h3 className="userName">{user.name}</h3>
+      <div className="user-box">
+        {/* bug fix : google image dosent load */}
+        {/* <img src={photoURL} alt="Google User" className="user-image" /> */}
+        <h2 className="greeting">Welcome</h2>
+        <h3 className="userName">{user.name}</h3>
+      </div>
     </div>
   );
 }
